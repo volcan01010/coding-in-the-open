35e7e1fc2ca918e97ff9916d498c393a15c3268e
John A Stevenson <jostev@bgs.ac.uk>
2021-06-14 17:29:23 +0100

    Sort source repos by creation date

--- misc/gitlab_query.py
+++ misc/gitlab_query.py
@@ -1,7 +1,9 @@
 # coding: utf-8
+from pprint import pprint
 import requests
 
 result = requests.get('https://api.github.com/orgs/BritishGeologicalSurvey/repos',
-                      params={'per_page': 100, 'type': 'forks'})
+                      params={'per_page': 100, 'type': 'source'})
 start_dates = {item['name']: item['created_at'] for item in result.json()}
-print(start_dates)
+start_dates = sorted(start_dates.items(), key=lambda x: x[1])
+pprint(start_dates)
